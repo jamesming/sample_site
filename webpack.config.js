490d96102a941c07dc7341270b3cd490adefd0ae
jamesming <jamesming@gmail.com>
2016-12-30 06:13:08 -0800

    Update

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  devtool: "inline-sourcemap",
+  entry: "./src/js/app.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+        }
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/public",
+    filename: "app.min.js"
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: true, sourcemap: false }),
+  ],
+};
